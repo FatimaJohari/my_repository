b391e75e0479bd8dd0ba2df0a56009c6fa421364
Fatima <fatemeh.johari@angstrom.uu.se>
2020-03-31 18:38:21 +0200

    debugged file

--- Day2/buggy/dicegame/runner.py
+++ Day2/buggy/dicegame/runner.py
@@ -15,18 +15,28 @@ class GameRunner:
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            value = int(die.value)
+            total += value
         return total
 
     @classmethod
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
+        # to count the consequensive wins
         c = 0
+        
+        # count rounds
+        r = 0
+
+        #count wins and loses
+        wins = 0
+        loses = 0
+
         while True:
             runner = cls()
 
-            print("Round {}\n".format(runner.round))
+            print("Round {}\n".format(runner.round + r))
 
             for die in runner.dice:
                 print(die.show())
@@ -36,16 +46,18 @@ class GameRunner:
 
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
-                runner.wins += 1
+                wins += 1
                 c += 1
+                r += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
-                runner.loses += 1
+                loses += 1
                 c = 0
-            print("Wins: {} Loses {}".format(runner.wins, runner.loses))
-            runner.round += 1
+                r +=1
+            print("Wins: {} Loses {}".format(runner.wins + wins, runner.loses +loses))
+            #runner.round += 1
 
             if c == 6:
                 print("You won... Congrats...")
@@ -57,4 +69,5 @@ class GameRunner:
             if prompt == 'y' or prompt == '':
                 continue
             else:
-                i_just_throw_an_exception()
+                break
+                #i_just_throw_an_exception()
