248417bfd0ff1884b6ab54912e8cfda90ee21f73
Fatima <fatemeh.johari@angstrom.uu.se>
2020-03-31 10:40:18 +0200

    exercise 2 part one

---
+++ Day2/animals/dangerous/fish.py
@@ -0,0 +1,9 @@
+class Fish:
+
+    def __init__(self):
+        self.members = ['Fish1', 'Fish2', 'Fish3']
+
+    def printMembers(self):
+        print('Printing members of the Fish class')
+        for member in self.members:
+            print('\t%s' % member)
