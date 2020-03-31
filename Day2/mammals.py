97cfab8ea15426b1c7e4ff62c3fdf339de981745
Fatima <fatemeh.johari@angstrom.uu.se>
2020-03-31 09:12:57 +0200

    mammals

---
+++ Day2/mammals.py
@@ -0,0 +1,8 @@
+class Mammals:
+    def __init__(self):
+        self.members = ['Tiger','Elephant','Wild Cat']
+
+    def printMembers (self):
+        print('Printing the members of Mammals class')
+        for member in self.members:
+            print ('/t%s ' % member)
